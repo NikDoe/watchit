32a65d4a36fce2961159332e1fe9503c4f2afba7
NikDoe <nikd0e@yandex.ru>
2022-07-12 18:22:01 +0300

    init(watchit): initialize & project setup

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	useTabs: true,
+	trailingComma: 'all',
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	arrowParens: 'avoid',
+	bracketSameLine: false,
+	bracketSpacing: true,
+};
