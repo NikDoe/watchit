bdda7825c51868394961dd697f7511c2b9ecfb84
NikDoe <nikd0e@yandex.ru>
2022-07-12 19:59:08 +0300

    fix(watchit): this fixes #1, w/ lodash debounce function

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
 const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+
+const start = debounce(() => {
+	console.log('USER PROGRAM STARTED');
+}, 100);
 
 chokidar
 	.watch('.')
-	.on('add', () => console.log('file added'))
+	.on('add', start)
 	.on('change', () => console.log('file changed'))
 	.on('unlink', () => console.log('file remove'));
