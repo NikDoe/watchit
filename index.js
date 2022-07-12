cf87b84e963a37c8ea8a36ce70686c462f5ed53f
NikDoe <nikd0e@yandex.ru>
2022-07-12 19:41:05 +0300

    feature(watchit): watchinh files w/ chokidar

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('i watch this');
+const chokidar = require('chokidar');
+
+chokidar
+	.watch('.')
+	.on('add', () => console.log('file added'))
+	.on('change', () => console.log('file changed'))
+	.on('unlink', () => console.log('file remove'));
